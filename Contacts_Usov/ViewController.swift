0be1d0296114080135bbc4f2dbec63eb7921977e
MrMurman <storm2150@yandex.ru>
2021-06-30 09:22:37 +0300

    Initial Commit

---
+++ Contacts_Usov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts_Usov
+//
+//  Created by Андрей Бородкин on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
